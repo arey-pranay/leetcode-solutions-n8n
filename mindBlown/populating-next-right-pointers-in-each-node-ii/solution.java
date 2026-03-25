09ab07f05fe07eb56f81d992a2aec666a00f4738
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-03-25 19:41:10 +0530

    Add solution: Populating Next Right Pointers In Each Node Ii

---
+++ mindBlown/populating-next-right-pointers-in-each-node-ii/solution.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int sz = q.size();
+            Node prev = null;
+            Node curr = null;
+            for(int i=0;i<sz;i++){
+                curr = q.poll();
+                if(i>0) prev.next = curr;
+                if(curr.left != null) q.offer(curr.left);
+                if(curr.right != null) q.offer(curr.right);
+                prev = curr;
+            }
+            curr.next = null;
+        }
+        return root;
+    }
+}
