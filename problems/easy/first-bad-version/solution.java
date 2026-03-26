a375dcddc814a95c6b499bec7ec652e934ea2dc3
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-03-27 00:02:05 +0530

    Add solution: First Bad Version

---
+++ problems/easy/first-bad-version/solution.java
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int first = 1; 
+        int last = n;
+        int ans =0;
+        while(first<=last){
+            int m = first+(last-first)/2;
+            if(isBadVersion(m)) {
+                ans = m;
+                last = m-1;
+            }
+            else first = m+1 ;
+        }
+        return ans ;
+    }
+}
