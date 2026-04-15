dff377e479d815c49924f1616bdc1cb40e9cf6bc
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-04-15 12:44:48 +0530

    Add solution: Shortest Distance To Target String In A Circular Array

--- dailyQues/04/shortest-distance-to-target-string-in-a-circular-array/solution.java
+++ dailyQues/04/shortest-distance-to-target-string-in-a-circular-array/solution.java
@@ -2,14 +2,13 @@ class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
         int n = words.length;
         for(int i=0;i<=n/2;i++){
-            if(words[insideBound(startIndex - i,n)].equals(target) ||  
-                words[insideBound(startIndex + i,n)].equals(target)) 
+            if(words[bound(startIndex - i,n)].equals(target) ||  
+                words[bound(startIndex + i,n)].equals(target)) 
             return i;
         }
         return -1;
     }
-    public int insideBound(int i, int n){
-        if(i<n) i = (i % n + n) % n;
-        return i%n;
+    public int bound(int i, int n){
+        return (i % n + n) % n;
     }
 }
