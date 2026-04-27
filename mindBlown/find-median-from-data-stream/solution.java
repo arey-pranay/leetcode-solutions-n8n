8249b603a20fc1f7a5bf0c7c3169c5923cbd6006
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-04-27 20:20:14 +0530

    Add solution: Find Median From Data Stream

---
+++ mindBlown/find-median-from-data-stream/solution.java
@@ -0,0 +1,32 @@
+class MedianFinder {
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private boolean even = true;
+
+    public double findMedian() {
+        if (even) return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        else return maxHeap.peek();
+    }
+
+// 1 2 5 3 4 8 6 7
+
+// min = 5 3
+// max = 1 2
+    public void addNum(int num) {
+        if (even) {
+            minHeap.offer(num);
+            maxHeap.offer(minHeap.poll());
+        } else {
+            maxHeap.offer(num);
+            minHeap.offer(maxHeap.poll());
+        }
+        even = !even;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
