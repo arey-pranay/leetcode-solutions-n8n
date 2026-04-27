b09a28b62772c2ec9a442b4b07e7e5e987500800
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-04-27 21:12:26 +0530

    Add solution: Single Number Ii

--- top150/bits/single-number-ii/solution.java
+++ top150/bits/single-number-ii/solution.java
@@ -1,15 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if (nums.length == 1) return nums[0];
+        if(nums.length == 1) return nums[0];
         int res = 0;
-        for (int i = 0; i < 32; i++) {
+        for(int i=0;i<32;i++){
             int ones = 0;
-            for (int j = 0; j < nums.length; j++) {
-                ones += nums[j] & 1;
-                nums[j] >>= 1;
-            }
-            res |= (ones % 3) << i;  // place bit at position i directly
+            for(int j=0;j<nums.length;j++){
+                ones += nums[j]&1;
+                nums[j]>>=1;
+            } 
+            res = (res | ((ones%3) << i)); // res ke right se ith bit pe 1 ya 0 rkhna hai
         }
+        // 000001 | (1<<2 ) -> 00001 | (100) -> 101 
         return res;
     }
 }
+
