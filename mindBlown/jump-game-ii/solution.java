9531ffb86e41ea9974c47690f4914f78e6864f21
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-07 20:00:52 +0530

    Add solution: Jump Game Ii

--- mindBlown/jump-game-ii/solution.java
+++ mindBlown/jump-game-ii/solution.java
@@ -1,16 +1,37 @@
 class Solution {
-    int n ;
-    int[] memo ;
     public int jump(int[] nums) {
-        n = nums.length;
-        memo = new int[n];
-        Arrays.fill(memo,10001);
-        return func(0,nums);
-    }
-    public int func(int i , int[] nums){
-        if(i>=n-1) return 0;
-        if(memo[i]!=10001) return memo[i];
-        for(int j=1;j<=nums[i];j++)  memo[i]=Math.min(memo[i],1+func(i+j,nums));
-        return memo[i];
+
+        int jumps = 0;
+        int currEnd = 0;
+        int farthest = 0;
+
+        for(int i = 0; i < nums.length - 1; i++) {
+
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if(i == currEnd) {
+                jumps++;
+                currEnd = farthest;
+            }
+        }
+
+        return jumps;
     }
 }
+
+// class Solution {
+//     int n ;
+//     int[] memo ;
+//     public int jump(int[] nums) {
+//         n = nums.length;
+//         memo = new int[n];
+//         Arrays.fill(memo,10001);
+//         return func(0,nums);
+//     }
+//     public int func(int i , int[] nums){
+//         if(i>=n-1) return 0;
+//         if(memo[i]!=10001) return memo[i];
+//         for(int j=1;j<=nums[i];j++)  memo[i]=Math.min(memo[i],1+func(i+j,nums));
+//         return memo[i];
+//     }
+// }
