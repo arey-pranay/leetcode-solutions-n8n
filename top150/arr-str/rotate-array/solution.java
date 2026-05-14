178feb063e11c96a30b293cf6cad180010ef7c33
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-14 20:56:03 +0530

    Add solution: Rotate Array

---
+++ top150/arr-str/rotate-array/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k%=n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+    public void reverse(int[] arr, int s, int e){
+        while(s<e) swap(arr,s++,e--);
+    }
+    public void swap(int[] arr, int s, int e){
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp; 
+    }
+}
