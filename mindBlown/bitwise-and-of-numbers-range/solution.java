fa2dfb92e6eaa783ba06f3bde4b5a69b31530dca
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-14 19:36:52 +0530

    Add solution: Bitwise And Of Numbers Range

--- mindBlown/bitwise-and-of-numbers-range/solution.java
+++ mindBlown/bitwise-and-of-numbers-range/solution.java
@@ -6,6 +6,20 @@ class Solution {
         return right;
     }
 }
+
+// class Solution {
+//     public int rangeBitwiseAnd(int left, int right) {
+//         int cnt = 0;
+//         while (left != right) {
+//             left >>= 1;
+//             right >>= 1;
+//             cnt++;
+//         }
+//         return (left << cnt);
+//     }
+// }
+
+
 // Brian Kernighan trick
 // right isliye kyonki right se and p bada difference m no. gyb hoga , lekin left kyonki chhoti value hai to bada difference m numbero ka gyb hona ho ni paayge , paryapt ni gyb honge 
 // ab gyb kyon kese ho rhe hai - > aur unke hone naa hona equal kese hai ?? -> kyonki 2 no. pados k & ho rhe hai , unse jo 1's 0 ban gye to ab iske sath kuchh bhi kabhi 1 ni hoga 
