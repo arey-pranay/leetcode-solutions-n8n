d6545b8c9a571b03e48c92b171fcc7a5b40af73f
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-14 20:49:09 +0530

    Add solution: Majority Element

---
+++ top150/arr-str/majority-element/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int curr = nums[0];
+        int count = 0;
+        for(int num : nums){
+            if(count == 0) curr = num;
+            if(curr == num) count++;
+            else count--;
+        }
+        return curr;
+    }
+}
