2af90fac0e8c2e14bdcb598a61a517bf05843d7d
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-19 20:12:57 +0530

    Add solution: Insert Delete Getrandom O1

--- mindBlown/insert-delete-getrandom-o1/solution.java
+++ mindBlown/insert-delete-getrandom-o1/solution.java
@@ -1,15 +1,15 @@
 class RandomizedSet {
     HashMap<Integer,Integer> hm;
-    ArrayList<Integer> al ;
+    ArrayList<Integer> arr;
     public RandomizedSet() {
         hm = new HashMap<>();
-        al = new ArrayList<>();
+        arr = new ArrayList<>();
     }
     
     public boolean insert(int val) {
         if(hm.containsKey(val)) return false;
-        hm.put(val,al.size());
-        al.add(val);
+        hm.put(val,arr.size());
+        arr.add(val);
         return true;
     }
     
@@ -18,19 +18,19 @@ class RandomizedSet {
 
         // removal we will do only from end of array because that is o(1), so we will keep the last element at the toBeDeleted index, and then remove the last element. then update the hashmap accordingly.
         int index = hm.get(val);
-        int lastVal = al.get(al.size() - 1);
-        al.set(index,lastVal);
-        al.remove(al.size()-1);
+        int lastVal = arr.get(arr.size() - 1);
+        arr.set(index,lastVal);
+        arr.remove(arr.size()-1);
 
         hm.put(lastVal,index);
         hm.remove(val);
-        
+
         return true;
     }
     
     public int getRandom() {
-        int random = (int)(Math.random() * al.size());
-        return al.get(random);
+        int random = (int)(Math.random() * arr.size());
+        return arr.get(random);
     }
 }
 
