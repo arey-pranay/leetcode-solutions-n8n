9b3f6d22d370c8aef9299f996db4c28c5118b6aa
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-20 20:46:23 +0530

    Add solution: Trapping Rain Water

--- mindBlown/trapping-rain-water/solution.java
+++ mindBlown/trapping-rain-water/solution.java
@@ -1,21 +1,14 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int[] lmax = new int[n];
-        int[] rmax = new int[n];
-        
-        lmax[0] = height[0];
-        for(int i=1;i<n;i++) lmax[i] = Math.max(lmax[i-1],height[i-1]);
-        rmax[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--) rmax[i] = Math.max(rmax[i+1],height[i+1]);
-        
-        
+        int[] pre = new int[n];
+        int[] suf = new int[n]; 
+        pre[0] = height[0];
+        suf[n-1] = height[n-1];
         int ans = 0;
-        for(int i =0; i<n;i++){
-            if(lmax[i] <= height[i] || rmax[i] <= height[i]) continue;
-            int canStore = Math.min(lmax[i],rmax[i]);
-            ans += canStore-height[i];
-        }
-        return ans;
+        for(int i=1;i<n;i++) pre[i] = Math.max(pre[i-1],height[i]);
+        for(int i=n-2;i>=0;i--) suf[i] = Math.max(suf[i+1],height[i]);
+        for(int i=0;i<n;i++) ans += Math.min(pre[i],suf[i]) - height[i];
+        return ans;        
     }
 }
