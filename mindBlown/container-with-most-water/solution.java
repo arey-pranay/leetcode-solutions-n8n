92c613d1b609aa18e107edb0f5f701535970125d
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-26 20:25:05 +0530

    Add solution: Container With Most Water

---
+++ mindBlown/container-with-most-water/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int i=0;
+        int j = n-1;
+        int max = Integer.MIN_VALUE; 
+        while(i<j){
+            max = Math.max(max,Math.min(height[i],height[j])*(j-i));
+            if(height[i] < height[j]) i++; else j--;
+        }
+        return max;
+    }
+}
