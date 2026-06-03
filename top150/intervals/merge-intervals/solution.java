6b0c63ea7165381760aba7d8b8019bb436991b1d
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-06-03 21:19:19 +0530

    Add solution: Merge Intervals

--- top150/intervals/merge-intervals/solution.java
+++ top150/intervals/merge-intervals/solution.java
@@ -1,20 +1,16 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        ArrayList<int[]> ans = new ArrayList<>();
-        int n = intervals.length;
-        int j = 0;
-        ans.add(intervals[0]);
+        int curr = 0;
+        int  n = intervals.length;
         for(int i=1;i<n;i++){
-            int[] curr = ans.get(j);
-            int[] next = intervals[i];
-            if(curr[1] > next[1]) i+=0;
-            else if(curr[1] >= next[0])curr[1]=next[1];
-            else{ans.add(next);j++;} 
+            if(intervals[curr][1] >= intervals[i][0]){ // merge
+                intervals[curr][1] = Math.max(intervals[i][1], intervals[curr][1]);
+            } else {
+                curr++;
+                intervals[curr] = intervals[i]; // copy it ahead kyuki uspe updated intervals aana chaiye for later comparison(s)
+            }
         }
-        int[][] arr = new int[ans.size()][2];
-        int i=0;
-        for(int[] temp : ans) arr[i] = ans.get(i++);
-        return arr;
+        return Arrays.copyOfRange(intervals,0,curr+1);
     }
 }
