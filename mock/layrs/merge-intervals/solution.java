188de48af860b908e395ae202f0c93200de7ef28
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-06-05 19:48:45 +0530

    Add solution: Merge Intervals

---
+++ mock/layrs/merge-intervals/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int curr = 0;
+        int  n = intervals.length;
+        for(int i=1;i<n;i++){
+            if(intervals[curr][1] >= intervals[i][0]){ // merge
+                intervals[curr][1] = Math.max(intervals[i][1], intervals[curr][1]);
+            } else {
+                curr++;
+                intervals[curr] = intervals[i];
+            }
+        }
+        return Arrays.copyOfRange(intervals,0,curr+1);
+    }
+}
