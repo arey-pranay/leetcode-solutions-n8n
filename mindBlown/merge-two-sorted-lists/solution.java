e1c21e6384e92e935560a13640f86620d518a8ff
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-06-09 21:21:17 +0530

    Add solution: Merge Two Sorted Lists

--- mindBlown/merge-two-sorted-lists/solution.java
+++ mindBlown/merge-two-sorted-lists/solution.java
@@ -11,8 +11,7 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null && list2 == null) return null;
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
+        if(list1 == null) return list2; if(list2 == null) return list1;
         if(list2.val < list1.val) return mergeTwoLists(list2,list1);
         ListNode head = list1;
         list1 = list1.next;
@@ -20,3 +19,45 @@ class Solution {
         return head;
     }
 }
+
+// 1 3 4
+// 2 4
+
+// 1 -> (...) // 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> null
+
+// -----------
+
+// 3 4 
+// 2 4
+
+// 1 -> (...) // 1 -> 2 -> 3 -> 4 -> 4 -> null
+// -------------
+
+// 4 
+// 3 4
+
+// 2 -> (...) // 2 -> 3 -> 4 -> 4 -> null
+
+// ---------------
+
+// 4
+// 4
+
+// 3 -> (...) // 3 -> 4 -> 4 -> null
+
+// ------------------
+
+// _
+// 4
+
+// 4 -> (...) // 4 -> 4 -> null
+
+// -------------
+
+// _
+// _
+
+// 4 ->(...) // 4 -> null
+
+// ------------
+
