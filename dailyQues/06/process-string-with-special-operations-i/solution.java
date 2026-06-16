fb2dd32870848953e0cef439d56760402955b75f
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-06-16 14:11:08 +0530

    Add solution: Process String With Special Operations I

---
+++ dailyQues/06/process-string-with-special-operations-i/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c=='*'){if(!sb.isEmpty()) sb.deleteCharAt(sb.length()-1);}
+            else if(c=='#') sb.repeat(sb,1);
+            else if(c=='%') sb.reverse();
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+}
