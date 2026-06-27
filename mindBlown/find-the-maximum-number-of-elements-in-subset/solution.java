e2925240704b5a1c0234f5ead6e913bcc66038fb
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-06-27 12:22:21 +0530

    Add solution: Find The Maximum Number Of Elements In Subset

--- mindBlown/find-the-maximum-number-of-elements-in-subset/solution.java
+++ mindBlown/find-the-maximum-number-of-elements-in-subset/solution.java
@@ -3,19 +3,13 @@ class Solution {
     HashSet<Integer> doubles = new HashSet<>();
     int ans = 1;
     public int maximumLength(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
-        int prev = nums[0];
-        int ones = prev == 1 ? 1 : 0;
-        for(int i=1;i<n;i++){
+        int ones = 0;
+        for(int i=0;i<n;i++){
             if(nums[i]==1) ones++;
-            if(nums[i] == prev) doubles.add(nums[i]);
-            else {
-                if(!doubles.contains(prev)) singles.add(prev);
-                prev = nums[i];
-            }
+            if(!singles.contains(nums[i])) singles.add(nums[i]);
+            else {singles.remove(nums[i]); doubles.add(nums[i]);}
         }
-        if(!doubles.contains(prev)) singles.add(prev);
         ones = ones%2 == 0 ? ones-1 : ones;
         ans = Math.max(ans, ones);
         for(int num : doubles){
@@ -27,8 +21,9 @@ class Solution {
     public void func(int curr, int toFind){
         if(!singles.contains(toFind) && !doubles.contains(toFind)) return;
         ans = Math.max(ans,curr+1);
-        if(doubles.contains(toFind)) func(curr+2, (int)Math.pow(toFind,2));
+        if(doubles.contains(toFind))func(curr+2, (int)Math.pow(toFind,2));
     }
+}
     // 2 and find 2
     // 22 and find 4
     // 242 and return 
