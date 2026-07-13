a08bc4492e67a660fbe2207566a22bb4695af323
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-07-13 20:32:31 +0530

    Add solution: Trapping Rain Water

--- mindBlown/trapping-rain-water/solution.java
+++ mindBlown/trapping-rain-water/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int[] pre = new int[n];
-        int[] suf = new int[n]; 
-        pre[0] = height[0];
-        suf[n-1] = height[n-1];
         int ans = 0;
-        for(int i=1;i<n;i++) pre[i] = Math.max(pre[i-1],height[i]);
-        for(int i=n-2;i>=0;i--) suf[i] = Math.max(suf[i+1],height[i]);
-        for(int i=0;i<n;i++) ans += Math.min(pre[i],suf[i]) - height[i];
-        return ans;        
+        int n = height.length;
+        int[] maxL = new int[n];
+        int[] maxR = new int[n];
+        maxL[0] = height[0];
+        maxR[n-1] = height[n-1];
+        for(int i=1;i<n;i++) maxL[i] = Math.max(maxL[i-1],height[i]);
+        for(int i=n-2;i>=0;i--) maxR[i] = Math.max(maxR[i+1],height[i]);
+        for(int i=0;i<n;i++) ans +=  Math.min(maxL[i], maxR[i])-height[i];
+        return ans;
     }
 }
+
+// [0,1,0,2,1,0,1,3,2,1,2,1]
