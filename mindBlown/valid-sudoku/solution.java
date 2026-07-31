c3cb7b24c2d592aeadb9ae070fee894a24967138
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-07-31 21:08:45 +0530

    Add solution: Valid Sudoku

--- mindBlown/valid-sudoku/solution.java
+++ mindBlown/valid-sudoku/solution.java
@@ -1,45 +1,16 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return checkRows(board)&&checkCols(board)&& checkCells(board);
-    }
-    public boolean checkCells(char[][] board){
-        for(int i=0;i<=6;i+=3){
-            for(int j=0; j<=6; j+=3){
-                if(!checkCell(board,i,j)) return false;
-            }
-        }
-        return true;
-    }
-    public boolean checkCell(char[][] board, int x, int y){
-        HashSet<Character> hs = new HashSet<>();
-        for(int i=x;i<x+3;i++){
-            for(int j=y;j<y+3;j++){
-                if(board[i][j] == '.') continue;
-                if(hs.contains(board[i][j])) return false;
-                hs.add(board[i][j]);
-            }
-        }
-        return true;
-    }
-    public boolean checkCols(char[][] board){
-        for(int i=0;i<9;i++){
-            HashSet<Character> hs = new HashSet<>();
-            for(int j =0;j<9;j++){
-                if(board[i][j] == '.') continue;
-                if(hs.contains(board[i][j])) return false;
-                hs.add(board[i][j]);
-            }
-        }
-        return true;
-    }  
-    public boolean checkRows(char[][] board){
-        for(int i=0;i<9;i++){
-            HashSet<Character> hs = new HashSet<>();
-            for(int j =0;j<9;j++){
-                if(board[j][i] == '.') continue;
-                if(hs.contains(board[j][i])) return false;
-                hs.add(board[j][i]);
-            }
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+        for(int i=0;i<board.length;i++){
+          for(int j=0; j<board[0].length; j++){
+            if(board[i][j]=='.') continue;
+            int num = board[i][j]-'1';
+            int box = (i/3)*3 + (j/3);
+            if(rows[i][num] || cols[j][num] || boxes[box][num]) return false;
+            rows[i][num] = cols[j][num] = boxes[box][num] = true;
+          }
         }
         return true;
     }
