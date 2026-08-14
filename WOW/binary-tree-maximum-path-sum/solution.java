917c9775f1cb4265d9114c19fc854bfa5696fd45
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-08-14 20:29:50 +0530

    Add solution: Binary Tree Maximum Path Sum

--- WOW/binary-tree-maximum-path-sum/solution.java
+++ WOW/binary-tree-maximum-path-sum/solution.java
@@ -1,17 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int ans = Integer.MIN_VALUE;
+    int ans =Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
         func(root);
         return ans;
     }
-
     public int func(TreeNode root){
         if(root==null) return 0;
-        int leftMax = Math.max(0,func(root.left));
-        int rightMax = Math.max(0,func(root.right));
-        ans = Math.max(ans, root.val+rightMax+leftMax);
-       // parent se kahege ki if you take me-> then the best I can do is:
-        return root.val + Math.max(leftMax , rightMax); 
+        int lsum = Math.max(0,func(root.left));
+        int rsum = Math.max(0,func(root.right));
+        ans = Math.max(ans,lsum+rsum+root.val);
+        return root.val+Math.max(lsum,rsum); 
+        //kyuki mera parent, mere through -> ye est path le skta hai. (me + leftSubtree) or (me+rightSubTree)
     }
-  
 }
