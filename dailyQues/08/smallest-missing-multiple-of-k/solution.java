2e830126d518fae2fd5fdb8757dc252c521941d2
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-08-25 10:06:38 +0530

    Add solution: Smallest Missing Multiple Of K

---
+++ dailyQues/08/smallest-missing-multiple-of-k/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) set.add(num);
+        int ans = k; 
+        for(int num : set)if(set.contains(ans)) ans+=k;
+        return ans;
+    }
+}
