391fd5310dc4b8e132090daf8d3f1f57e309136e
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-08-27 21:11:49 +0530

    Add solution: Reverse Nodes In K Group

--- WOW/reverse-nodes-in-k-group/solution.java
+++ WOW/reverse-nodes-in-k-group/solution.java
@@ -1,42 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        int length = 0;
-        ListNode first = head;
-        while(first!=null){
-            length++;
-            first = first.next;
-        }
-        length -= length%k;
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        ListNode curr = dummy;
-        for(int i=0;i<length;i+=k){
-            ListNode[] arr = revRange(curr.next,k); // yaha curr.next se call , mtlb he
-            curr.next = arr[0];
-            curr = arr[1];
+        
+        ListNode temp = head;
+        int total = 0;
+        while(temp!=null){temp=temp.next;total++;}
+        
+        temp = dummy;
+        total -= (total%k);
+        for(int i=0;i<total;i+=k){
+          ListNode[] ans = rev(temp.next,k); 
+          temp.next =  ans[0];
+          temp = ans[1];
         }
+        
         return dummy.next;
     }
-    public ListNode[] revRange(ListNode head,int k){
-        ListNode prev = null;
-        ListNode curr = head;
-        while(k-->0){
-            ListNode near = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = near;
-        }
-        head.next = curr;
-        return new ListNode[]{prev,head};
+    public ListNode[] rev(ListNode head , int k){
+      ListNode curr = head;
+      ListNode prev = null;
+      while(k-->0){
+        ListNode fwd = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = fwd;
+      }
+      head.next = curr;
+      return new ListNode[]{prev,head};
     }
-}
+}    
