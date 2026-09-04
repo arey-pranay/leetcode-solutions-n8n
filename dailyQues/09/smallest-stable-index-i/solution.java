f3ee9993acdf05675b42204ac5ea7159a041c284
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-09-04 10:24:30 +0530

    Add solution: Smallest Stable Index I

---
+++ dailyQues/09/smallest-stable-index-i/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int min[] = new int[n];
+        int max[] = new int[n];
+        max[0] = nums[0];
+        min[n-1] = nums[n-1];
+        for(int i = 1;i<n;i++){max[i] = Math.max(nums[i],max[i-1]); min[n-1-i] = Math.min(nums[n-1-i],min[n-i]);}
+        for(int i = 0;i<n;i++)if(max[i]-min[i] <= k) return i;
+        return -1;
+        
+        
+    }
+}
