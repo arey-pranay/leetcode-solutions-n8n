bf3116991e501054847d1d347ef2741100197101
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-09-24 15:25:58 +0530

    Add solution: Smallest Index With Digit Sum Equal To Index

---
+++ dailyQues/09/smallest-index-with-digit-sum-equal-to-index/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        for(int i=0;i<nums.length;i++) if(digitSum(nums[i])==i) return i;
+        return -1;
+    }
+    public int digitSum(int num){
+        int sum=0;
+        while(num>0){
+            int rem = num%10;
+            num/=10;
+            sum += rem;
+        }
+        return sum;
+    }
+}
